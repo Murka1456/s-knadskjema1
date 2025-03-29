2876ad0e96c735f111f48b55e67b8dfe17a5ec65
Murka1456 <mal-bod2@hotmail.com>
2025-03-29 19:08:51 +0100

    kjønn valgt

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const addTodo = document.querySelector("#addTodo");
 addTodo.addEventListener("click",function(event)){
-  Eventpreventdefault();
+  Event.preventDefault();
   console.log("Knappen er trykket !");
 }
+const kjønn = document.querySelector("#kjønn");
+kjønn.addEventListener("velg", function(event)){
+  console.log("kjønn valgt")
+};
+
